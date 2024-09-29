7f7872dd1852134dd084e1694fd5bb439c957d1f
Dinazzlle <dinazzlle@gmail.com>
2024-09-30 00:18:56 +0300

    file 1 updated

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,5 +1,3 @@
-# arithmetic_operations.py
-
 def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 + num2
@@ -8,9 +6,8 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        if num2 != 0:
-            return num1 / num2
-        else:
+        if num2 == 0:
             return "Error: Division by zero"
+        return num1 / num2
     else:
-        return "Invalid operation"
+        return "Error: Invalid operation"
